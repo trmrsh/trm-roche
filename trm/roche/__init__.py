654b5f82aab585d72899ea6ec158047a449a576e
Tom Marsh <t.r.marsh@warwick.ac.uk>
2008-11-28 11:07:04 +0000

    Updated documentation
    Documentation string now starts with a listing of the available
    functions.

--- trm/roche/__init__.py
+++ trm/roche/__init__.py
@@ -1,1 +1,26 @@
+"""
+Roche geometry routines
+
+This module provides some easy-to-use routines for computations
+to do with Roche geometry. All routines work in coordinates scaled
+by the binary separation, and time units such that the binary angular
+frequency = 1.
+
+Functions
+=========
+
+lobe1    -- the primary star's Roche lobe
+lobe2    -- the secondary star's Roche lobe
+stream   -- the gas stream
+strmnx   -- position and velocity of n-th turning point of gas stream
+vlobe1   -- the primary star's Roche lobe, velocity space
+vlobe2   -- the secondary star's Roche lobe, velocity space
+vstream  -- gas stream in velocity coordinates
+xl1      -- L1 position
+xl2      -- L2 position
+xl3      -- L3 position
+"""
+
+import sys
+sys.path.append('.')
 from _roche import *
