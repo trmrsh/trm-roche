5bd98c2bf2c1dc16f6be35ca892b264a9677272f
Tom Marsh <t.r.marsh@warwick.ac.uk>
2009-02-13 16:52:39 +0000

    Upgrades to roche
    New routines to compute eclipse of a particular point and
    to find the mass ratio given an inclination and phase width.

--- trm/roche/__init__.py
+++ trm/roche/__init__.py
@@ -4,22 +4,27 @@ Roche geometry routines
 This module provides some easy-to-use routines for computations
 to do with Roche geometry. All routines work in coordinates scaled
 by the binary separation, and time units such that the binary angular
-frequency = 1.
+frequency = 1. Various routines return 2D arrays which have the 
+advantage that they can be easily rotated. If you need to split them into
+x and y arrays then for example the output from stream can be set as
+
+x = r[0,:]
+y = r[1,:]
 
 Functions
 =========
 
-ingress_egress -- calculate the ingress/egress phase of a given point.
-lobe1          -- the primary star's Roche lobe
-lobe2          -- the secondary star's Roche lobe
-stream         -- the gas stream
-strmnx         -- position and velocity of n-th turning point of gas stream
-vlobe1         -- the primary star's Roche lobe, velocity space
-vlobe2         -- the secondary star's Roche lobe, velocity space
-vstream        -- gas stream in velocity coordinates
-xl1            -- L1 position
-xl2            -- L2 position
-xl3            -- L3 position
+ineg      -- calculate the ingress/egress phase of a given point.
+lobe1     -- the primary star's Roche lobe
+lobe2     -- the secondary star's Roche lobe
+stream    -- the gas stream
+strmnx    -- position and velocity of n-th turning point of gas stream
+vlobe1    -- the primary star's Roche lobe, velocity space
+vlobe2    -- the secondary star's Roche lobe, velocity space
+vstream   -- gas stream in velocity coordinates
+xl1       -- L1 position
+xl2       -- L2 position
+xl3       -- L3 position
 """
 
 import sys
